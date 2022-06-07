72c4b962622ff11411f1074b7bc0c0c42ced440a
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-07 02:58:35 +0000

    added function to get loopback address

--- src/net.rs
+++ src/net.rs
@@ -1,4 +1,7 @@
-use std::{io, net::TcpListener};
+use std::{
+    io,
+    net::{Ipv4Addr, SocketAddrV4, TcpListener},
+};
 
 pub fn get_unused_port() -> io::Result<u16> {
     // Binding to :0 requests that the operating system pick a free port for us.
@@ -6,4 +9,11 @@ pub fn get_unused_port() -> io::Result<u16> {
     let addr = listener.local_addr()?;
 
     Ok(addr.port())
+}
+
+pub fn get_loopback() -> io::Result<SocketAddrV4> {
+    let port = get_unused_port()?;
+    let ip_addr = Ipv4Addr::new(127, 0, 0, 1);
+
+    Ok(SocketAddrV4::new(ip_addr, port))
 }

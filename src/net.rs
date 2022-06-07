ffd531cab72c9ae88a8e4260da1121b09adc733a
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-07 02:56:03 +0000

    added comment

--- src/net.rs
+++ src/net.rs
@@ -1,6 +1,7 @@
 use std::{io, net::TcpListener};
 
 pub fn get_unused_port() -> io::Result<u16> {
+    // Binding to :0 requests that the operating system pick a free port for us.
     let listener = TcpListener::bind("127.0.0.1:0")?;
     let addr = listener.local_addr()?;
 

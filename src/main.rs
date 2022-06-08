eb7b09ac2836cc042e7db8766235a8636fcc1aca
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-08 00:44:44 +0000

    added testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod client;
 mod net;
 
+use std::net::TcpListener;
+
 use lazy_static::lazy_static;
 
 use client::credentials::ClientInfo;
@@ -9,8 +11,18 @@ lazy_static! {
     static ref CLIENT_INFO: ClientInfo = ClientInfo::new().unwrap();
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // Initialize client info straight away
     lazy_static::initialize(&CLIENT_INFO);
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+
+    println!("Listening on {}", listener.local_addr()?);
+
+    match listener.accept() {
+        Ok((_socket, addr)) => println!("new client: {addr:?}"),
+        Err(e) => println!("couldn't get client: {e:?}"),
+    }
+
+    Ok(())
 }

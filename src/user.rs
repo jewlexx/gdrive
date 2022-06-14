f61bea7049aa76f40d1e000a2c9b50561a49046f
Juliette Cordor <grassojames5@gmail.com>
2022-06-14 14:14:20 +1000

    fix

--- src/user.rs
+++ src/user.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserCredentials {
@@ -12,4 +13,30 @@ pub struct UserCredentials {
     pub scope: String,
     /// A token that you can use to obtain a new access token. Refresh tokens are valid until the user revokes access. Again, this field is only present in this response if you set the access_type parameter to offline in the initial request to Google's authorization server.
     pub refresh_token: String,
+}
+
+impl UserCredentials {
+    pub async fn get_credentials(
+        client_id: &str,
+        client_secret: &str,
+        user_code: &str,
+    ) -> Result<Self, reqwest::Error> {
+        let client_info = json!({
+            "grant_type": "authorization_code",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "code": user_code,
+        });
+
+        let response = reqwest::Client::new()
+            .post("https://oauth2.googleapis.com/token")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .json(&client_info)
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        Ok(response)
+    }
 }

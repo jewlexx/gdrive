e5b155b11b89ec417c7bc76a1ac226268cb97aa3
Juliette Cordor <grassojames5@gmail.com>
2022-06-14 13:22:58 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,72 +5,32 @@ use std::{
     convert::Infallible,
     io::{self, Read, Write},
     net::{SocketAddr, TcpListener, TcpStream},
-    sync::mpsc::channel,
 };
 
-use hyper::{
-    server::conn::AddrStream,
-    service::{make_service_fn, service_fn},
-    Body, Request, Response,
+use axum::{
+    extract::Query,
+    response::{IntoResponse, Redirect, Response},
+    routing::get,
+    Router,
 };
 use lazy_static::lazy_static;
+use tokio::sync::mpsc;
 
 use client::{credentials::ClientInfo, AUTH_ENDPOINT};
 use net::get_loopback;
 
-const RESPONSE: &[u8] = include_bytes!("response.html");
+type Sender = mpsc::UnboundedSender<RedirectQuery>;
+type Receiver = mpsc::UnboundedReceiver<RedirectQuery>;
 
 lazy_static! {
     static ref CLIENT_INFO: ClientInfo = ClientInfo::new().unwrap();
-}
-
-fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> io::Result<()> {
-    println!("Got connection");
-    let mut buf = Vec::<u8>::new();
-
-    // loop {
-    let n = stream.read(&mut buf)?;
-
-    // if n == 0 {
-    //     return Ok(());
-    // }
-
-    let request = String::from_utf8_lossy(&buf[..n]);
-    println!("Request: {}", request);
-
-    stream.write_all(RESPONSE)?;
-    // }
-
-    Ok(())
+    static ref CLOSE_SERVER: (Sender, Receiver) = mpsc::unbounded_channel::<RedirectQuery>();
 }
 
 #[derive(Debug)]
-struct Query {
+struct RedirectQuery {
     pub code: Option<String>,
-}
-
-impl Query {
-    pub fn from_query_string(query: &str) -> Result<Self, String> {
-        let opts = query.split('&');
-
-        let mut query = Self { code: None };
-
-        for opt in opts {
-            let mut kv = opt.split('=');
-
-            match kv.next() {
-                Some("code") => query.code = kv.last().map(String::from),
-                Some("error") => {
-                    if let Some(err) = kv.last() {
-                        return Err(String::from(err));
-                    }
-                }
-                _ => continue,
-            }
-        }
-
-        Ok(query)
-    }
+    pub error: Option<String>,
 }
 
 fn get_redirect() -> String {
@@ -92,43 +52,43 @@ async fn main() -> anyhow::Result<()> {
     // Initialize client info straight away
     lazy_static::initialize(&CLIENT_INFO);
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel::<u8>(8);
+    let app = Router::new()
+        .route("/", get(redirect))
+        .route("/callback", get(callback));
 
     // let listener = TcpListener::bind("127.0.0.1:0")?;
 
-    {
-        let tx = tx.clone();
-        ctrlc::set_handler(move || {
-            tx.blocking_send(1).unwrap();
-        })?;
-    }
-
-    let svc = make_service_fn(|socket: &AddrStream| async move {
-        Ok::<_, Infallible>(service_fn(move |req: Request<Body>| async move {
-            if req.uri() == "/" {
-                return Response::builder()
-                    .status(302)
-                    .header("Location", get_redirect())
-                    .body(Body::from(""));
-            }
-            let query_string = req.uri().query().unwrap_or("");
-            let query = Query::from_query_string(query_string);
-            println!("Got request");
-            Response::builder()
-                .status(200)
-                .body(Body::from(format!("Hello, {:?}!", query)))
-        }))
-    });
-
     let addr = get_loopback()?;
-    println!("Listening on http://{}", addr);
-    let server = hyper::Server::bind(&addr).serve(svc);
 
-    let with_grace = server.with_graceful_shutdown(async move {
-        rx.recv().await.expect("Failed to recieve");
-    });
+    tracing::debug!("Listening on http://{}", addr);
 
-    with_grace.await?;
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
 
     Ok(())
+}
+
+async fn redirect() -> impl IntoResponse {
+    let redirect_uri = get_redirect();
+
+    Redirect::temporary(&redirect_uri)
+}
+
+async fn callback(Query(query): Query<RedirectQuery>) -> impl IntoResponse {
+    if let Some(code) = query.code {
+        let client_info = RedirectQuery {
+            code: Some(code),
+            error: None,
+        };
+
+        CLOSE_SERVER.0.send(client_info).unwrap();
+
+        Response::new("Successfully redirected".into())
+    } else {
+        let body = include_str!("../public/error.html")
+            .replace("%error_msg%", &query.error.unwrap_or("invalid code".into()));
+
+        Response::new(body)
+    }
 }
